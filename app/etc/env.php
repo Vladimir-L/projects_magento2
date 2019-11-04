fca69e83cd4ebacf99a7d6cb22e0d24266d19270
Volodymyr Lopatkin <lopatkin.vladimir88@gmail.com>
2019-11-05 00:05:49 +0200

    Created new theme, added DB dump

---
+++ app/etc/env.php
@@ -0,0 +1,74 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'b1d5b201e9bcb0ff1a9c0c8ea1807c83'
+    ],
+    'db' => [
+        'table_prefix' => 'm2_',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql',
+                'dbname' => 'vladimir_lopatkin_local',
+                'username' => 'vladimir_lopatkin_local',
+                'password' => 'vladimir_lopatkin_local',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '40d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '40d_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 0,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Tue, 08 Oct 2019 10:32:08 +0000'
+    ],
+    'downloadable_domains' => [
+        'vladimir-lopatkin.local',
+        'example-website2.local'
+    ]
+];
---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,32 @@
+module.exports = {
+     vladimirl_luma_en_US: {
+        area: 'frontend',
+        name: 'Vladimirl/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    vladimirl_luma_uk_UA: {
+        area: 'frontend',
+        name: 'Vladimirl/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    vladimirl_luma_ru_RU: {
+        area: 'frontend',
+        name: 'Vladimirl/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+};
