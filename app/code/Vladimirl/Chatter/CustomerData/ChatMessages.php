81031eb23fbc07ba7fa6fe4095fcc94ba82703bb
Vladimir-L <lopatkin.vladimir88@gmail.com>
2020-03-30 08:31:28 +0300

    Fixed code style in composer json. Homework 21: Add customerData, di.xml, sections.xml, rename frontName in routes.xml,

---
+++ app/code/Vladimirl/Chatter/CustomerData/ChatMessages.php
@@ -0,0 +1,45 @@
+<?php
+declare(strict_types=1);
+
+namespace Vladimirl\Chatter\CustomerData;
+
+class ChatMessages implements \Magento\Customer\CustomerData\SectionSourceInterface
+{
+    /**
+     * @var \Magento\Customer\Model\Session $customerSession
+     */
+    private $customerSession;
+
+    /**
+     * @var \Vladimirl\Chatter\Model\ResourceModel\Collection\ChatMessageCollectionFactory $messageCollectionFactory
+     */
+    private $messageCollectionFactory;
+
+    /**
+     * ChatMessages constructor.
+     * @param \Magento\Customer\Model\Session $customerSession
+     * @param \Vladimirl\Chatter\Model\ResourceModel\Collection\ChatMessageCollectionFactory $messageCollectionFactory
+     */
+    public function __construct(
+        \Magento\Customer\Model\Session $customerSession,
+        \Vladimirl\Chatter\Model\ResourceModel\Collection\ChatMessageCollectionFactory $messageCollectionFactory
+    ) {
+        $this->customerSession = $customerSession;
+        $this->messageCollectionFactory = $messageCollectionFactory;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSectionData(): array
+    {
+        $chatHash = $this->customerSession->getChatHash();
+        $messageCollection = $this->messageCollectionFactory->create();
+        $messageCollection->addChatHashFilter($chatHash);
+        $list = array_reverse($messageCollection
+            ->setOrder('message_id', 'DESC')
+            ->setPageSize(10)
+            ->getColumnValues('message'));
+        return ['list' => $list];
+    }
+}
